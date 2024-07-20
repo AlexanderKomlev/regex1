187e25c6974dc8ddfbf09540eb383a2abaf0004f
Alexander Komlev <alexander.komlev42@gmail.com>
2024-07-20 14:38:01 +0700

    initial commit

---
+++ src/__tests__/app.js
@@ -0,0 +1,18 @@
+import Validator from '../app';
+
+test.each([
+  ['qwerty', true],
+  ['12345', false],
+  ['1qwerty', false],
+  ['qwerty1', false],
+  ['qwerty123zxcv', false],
+  ['qwerty12zxcv', true],
+  ['-qwerty', false],
+  ['qwerty-', false],
+  ['_qwerty', false],
+  ['qwerty_', false],
+  ['qwerty-zxcvb_12a', true],
+  ['a1a1a1a1a', true],
+])('Validate username: %s', (username, expected) => {
+  expect(Validator.validateUsername(username)).toBe(expected);
+});
---
+++ src/app.js
@@ -0,0 +1,8 @@
+export default class Validator {
+  static validateUsername(usernmame) {
+    const re1 = /^[^0-9-_][a-z0-9-_]+[^0-9-_]$/i;
+    const re2 = /[0-9]{3,}/;
+
+    return re1.test(usernmame) && !re2.test(usernmame);
+  }
+}
