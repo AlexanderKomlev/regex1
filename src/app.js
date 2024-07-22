4aeb79f28d8db8078f644c3496ce8a7c9069ad95
Alexander Komlev <alexander.komlev42@gmail.com>
2024-07-22 23:21:18 +0700

    fix app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 export default class Validator {
   static validateUsername(usernmame) {
     const re1 = /^[^0-9-_][a-z0-9-_]+[^0-9-_]$/i;
-    const re2 = /[0-9]{3,}/;
+    const re2 = /[0-9]{4,}/;
 
     return re1.test(usernmame) && !re2.test(usernmame);
   }
